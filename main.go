7922e5eeb2cd29d41cf98d28963e08e658753ed9
Brennon Gabriel de Oliveira <brennonoliveira20014@gmail.com>
2024-05-24 12:38:16 -0300

    Adicionando dockerfile e iniciando env

--- main.go
+++ main.go
@@ -2,9 +2,16 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+
+	err := godotenv.Load()
+	if err != nil {
+		panic("Error loading .env file")
+	}
+
 	// Create a new Gin router
 	router := gin.Default()
 
